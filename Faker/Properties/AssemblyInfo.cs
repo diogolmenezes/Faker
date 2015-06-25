89f1ba541903c1e789a950112f62be92d628211a
diogolmenezes <diogolmenezes@gmail.com>
2015-06-25 16:40:34 -0300

    improve

--- Faker/Properties/AssemblyInfo.cs
+++ Faker/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FakerTest")]
 [assembly: AssemblyDescription("Faker is a c# .net fake object generator like rails factory girl.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Diogo Menezes")]
 [assembly: AssemblyProduct("FakerTest")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
