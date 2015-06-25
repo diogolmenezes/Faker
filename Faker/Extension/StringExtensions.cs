d997f777516edfa0c8282a63ed18416e7d44f7f9
diogolmenezes <diogolmenezes@gmail.com>
2015-06-25 16:16:31 -0300

    nuget

---
+++ Faker/Extension/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace Faker.Extension
+{
+    public static class StringExtensions
+    {
+        public static bool IsMailField(this string fieldName)
+        {
+            string[] values = { "email", "mail" };
+
+            return fieldName != null && values.Contains(fieldName.ToLower());
+        }
+
+        public static bool IsNameField(this string fieldName)
+        {
+            string[] values = { "name", "nome", "fullname", "nomecompleto" };
+
+            return fieldName != null &&  values.Contains(fieldName.ToLower());
+        }
+
+        public static bool IsLoginField(this string fieldName)
+        {
+            string[] values = { "login" };
+
+            return fieldName != null &&  values.Contains(fieldName.ToLower());
+        }
+    }
+}
